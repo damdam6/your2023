89ccc4159c9913d7c17d50bca78c5d1c56cc327b
damongsanga <mike0413lata@gmail.com>
2023-12-20 22:57:58 +0900

    test: test 코드 수정

--- springboot project/imgMaker22/src/main/java/com/ssafy/imgMaker22/model/dto/Test.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.imgMaker22.model.dto;
-
-public class Test {
-}
--- springboot project/imgMaker22/src/test/java/com/ssafy/imgMaker22/keyword/VisionTest.java
+++ springboot project/imgMaker22/src/test/java/com/ssafy/imgMaker22/keyword/VisionTest.java
@@ -5,20 +5,20 @@ import com.ssafy.imgMaker22.model.dto.image.ImageGenerationResponseTest;
 import com.ssafy.imgMaker22.model.dto.image.PromptRequest;
 import com.ssafy.imgMaker22.model.dto.prompt.ImageRequest;
 import com.ssafy.imgMaker22.model.dto.prompt.PromptDTO;
-import com.ssafy.imgMaker22.model.service.ImageGenerationService;
-import com.ssafy.imgMaker22.model.service.KeywordGenerationService;
-import com.ssafy.imgMaker22.model.service.PromptService;
+import com.ssafy.imgMaker22.model.service.*;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ResourceLoader;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 @SpringBootTest
 @RequiredArgsConstructor
@@ -36,6 +36,11 @@ public class VisionTest {
     @Autowired
     private ImageGenerationService dalle3Service;
 
+    @Autowired
+    private FileService fileUploadService;
+
+    @Autowired
+    private S3Uploader s3Uploader;
 
     @Test
     void ImageGenerationTest() throws IOException {
@@ -46,14 +51,16 @@ public class VisionTest {
 
         Map<String, List<PromptDTO>> promptDTOMap = new HashMap<>();
 
-        List<PromptDTO> keywords = cloudVisionService.getKeywords(imageRequests);
+        List<PromptDTO> keywords = new ArrayList<>();
+        cloudVisionService.getKeywords(imageRequests).subscribe((promptDTO -> keywords.add(promptDTO)));
         int i = 1;
         System.out.println("keywords");
         for (PromptDTO p: keywords) {
             System.out.println(i++ + "  = " + p);
         }
 
-        List<PromptDTO> colors = cloudVisionService.getColors(imageRequests);
+        List<PromptDTO> colors = new ArrayList<>();
+        cloudVisionService.getColors(imageRequests).subscribe((promptDTO -> colors.add(promptDTO)));
         i = 1;
         System.out.println("colors");
         for (PromptDTO p: colors) {
@@ -89,6 +96,43 @@ public class VisionTest {
         } catch (Exception e){ // 수정
             e.printStackTrace(); // 수정
         }
+    }
+
+    @Test
+    void S3UploadTest() throws IOException {
+        Path imagePath = Path.of("/Users/djlata/Projects/your2023/springboot project/imgMaker22/src/main/resources/static/images/KakaoTalk_Photo_2023-12-19-01-26-47 001.jpeg");
+        // 이미지 파일을 바이트 배열로 읽기
+        byte[] imageBytes = Files.readAllBytes(imagePath);
+        // 바이트 배열을 Base64로 인코딩
+        String base64Encoded = Base64.getEncoder().encodeToString(imageBytes);
+        byte[] decodedBytes = java.util.Base64.getDecoder().decode(base64Encoded);
+
+        System.out.println(fileUploadTest(decodedBytes));
+        Assertions.assertThat(Arrays.toString(imageBytes)).isEqualTo(Arrays.toString(decodedBytes));
+    }
+
+    public String fileUploadTest(byte[] decodedBytes) throws IOException{
+        String storedFileUrl = null;
+        File file = convert(decodedBytes).get();
+        // 같은 파일명이 중복되는지 확인하는 로직이 필요함
+        storedFileUrl = s3Uploader.upload(file, "images"); // 경로명을 어떻게 해야 하지??
+        removeNewFile(file);
+        return storedFileUrl;
+    }
+
+    private Optional<File> convert(byte[] decodedBytes) throws IOException {
+        File file = new File("KakaoTalk_Photo_2023-12-19-01-26-47 001.jpeg"); // 이름 (경로) 가져오고 (수정필요)
+        if(file.createNewFile()) { // 같은 이름의 파일이 없다면
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                fos.write(decodedBytes); // 데이터 넣어주고
+            }
+            return Optional.of(file); // 리턴
+        }
+        return Optional.empty();
+    }
+
+    private void removeNewFile(File targetFile) {
+        targetFile.delete();
     }
 
 }

6266ddd1f4041da8afacf28c20bf4de11598cff7
damongsanga <mike0413lata@gmail.com>
2023-12-31 20:27:53 +0900

    feat: 요청 api 수정

--- springboot project/imgMaker22/src/main/java/com/ssafy/imgMaker22/controller/MainController.java
+++ springboot project/imgMaker22/src/main/java/com/ssafy/imgMaker22/controller/MainController.java
@@ -1,67 +1,63 @@
 package com.ssafy.imgMaker22.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.imgMaker22.model.dto.GeneratedImage;
-import com.ssafy.imgMaker22.model.dto.image.ImageGenerationResponse;
-import com.ssafy.imgMaker22.model.dto.image.PromptRequest;
-import com.ssafy.imgMaker22.model.dto.prompt.ImageRequest;
-import com.ssafy.imgMaker22.model.service.FileService;
-import com.ssafy.imgMaker22.model.service.ImageGenerationService;
-import com.ssafy.imgMaker22.model.service.KeywordGenerationService;
-import com.ssafy.imgMaker22.model.service.PromptService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.ssafy.imgMaker22.controller.dto.ImageResponse;
+import com.ssafy.imgMaker22.controller.dto.MakeImageRequest;
+import com.ssafy.imgMaker22.model.entity.GeneratedImage;
+import com.ssafy.imgMaker22.model.service.image.dto.ImageGenerationResponse;
+import com.ssafy.imgMaker22.model.service.prompt.dto.ImageUrlDto;
+import com.ssafy.imgMaker22.model.service.file.FileService;
+import com.ssafy.imgMaker22.model.service.image.ImageGenerationService;
+import com.ssafy.imgMaker22.model.service.prompt.PromptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/vision/api")
+@RequestMapping(value = "/api")
 @Slf4j
 public class MainController {
 
-    private PromptService promptService;
-    private ImageGenerationService dalle3Service;
-    private FileService fileService;
-    private final ObjectMapper mapper;
+    private final PromptService promptService;
+    private final ImageGenerationService dalle3Service;
+    private final FileService fileService;
 
+    @PostMapping("/image")
+    public ResponseEntity makeImage(@RequestBody MakeImageRequest requests) {
 
-    @PostMapping("/getImage")
-    public ResponseEntity makeImage(HttpServletRequest request, HttpServletResponse response, @RequestBody List<ImageRequest> imageRequests) {
-
-        String nickname = mapper.convertValue(request.getAttribute("nickname"), String.class);
-        String style = mapper.convertValue(request.getAttribute("style"), String.class);
+        String nickname = requests.getNickname();
+        String style = requests.getStyle();
         GeneratedImage gImage = GeneratedImage.builder().nickname(nickname).style(style).build();
 
-        List<ImageRequest> imageUrls = Arrays.stream(mapper.convertValue(request.getAttribute("images"), String[].class)).
-                map(s -> new ImageRequest(s)).collect(Collectors.toList());
-        PromptRequest promptRequest = PromptRequest.builder().prompt(promptService.makePrompt(imageUrls, style)).build();
+        List<ImageUrlDto> imageUrls = requests.getImages().stream().
+                map(s -> new ImageUrlDto(s.getUrl())).collect(Collectors.toList());
+        String generatedPrompt = promptService.makePrompt(imageUrls, style);
 
         // 이미지 생성 및 저장
         ImageGenerationResponse imageGenerationResponse = null;
         String url = null;
         try {
-            imageGenerationResponse = dalle3Service.makeImages(promptRequest);
+            imageGenerationResponse = dalle3Service.makeImages(generatedPrompt);
             byte[] decodedBytes = Base64.getDecoder().decode(imageGenerationResponse.getData().get(0).getB64Json());
             url = fileService.fileUpload(decodedBytes, gImage);
         } catch (Exception e) { // 수정
             log.error("error message : {}", e.getMessage());
         }
-        if (url == null) return new ResponseEntity<String>(url, HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<String>(url, HttpStatus.OK);
+
+        if (url == null) return new ResponseEntity<ImageResponse>(new ImageResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<ImageResponse>(new ImageResponse(url), HttpStatus.OK);
     }
 
-    @GetMapping("/getAll")
-    public ResponseEntity getAllImages(HttpServletRequest request, HttpServletResponse response) {
-        return new ResponseEntity<List<String>>(fileService.getAllFileUrl(), HttpStatus.OK);
+    @GetMapping("/allImages")
+    public ResponseEntity getAllImages() {
+        return new ResponseEntity<List<ImageResponse>>(
+                fileService.getAllFileUrl().stream().map(ImageResponse::new).collect(Collectors.toList()),
+                HttpStatus.OK);
     }
 }
